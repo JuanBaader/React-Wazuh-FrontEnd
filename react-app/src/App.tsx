82aab5c0c8b1976fdb6971eae6d90ba8a7efc854
juanm <jbaader@itba.edu.ar>
2022-11-22 23:39:37 -0300

    side bar and pages

---
+++ react-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Sidebar from './components/Sidebar'
+
+import Home from './pages/Home';
+import Team from './pages/Team';
+import Tasks from './pages/Tasks';
+
+
+const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/team' element={<Team />} />            
+          <Route path='/tasks' element={<Tasks />} />    
+        </Routes>
+      </Router> 
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/components/SidebarData.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa' 
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <FaIcons.FaHome />
+    },
+    {
+        title: 'Team',
+        path: '/team',
+        icon: <FaIcons.FaUsers />
+    },
+    {
+        title: 'Tasks',
+        path: '/tasks',
+        icon: <FaIcons.FaTasks />
+    },
+]
+
